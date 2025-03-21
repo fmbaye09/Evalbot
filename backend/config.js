17841487bc3958bbf6b604dcdf32b378074a0a67
fmbaye09 <126366429+fmbaye09@users.noreply.github.com>
2025-03-21 10:20:53 +0000

    Delete backend directory

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    development: {
-      username: 'exam_user',       // Nom d'utilisateur MySQL
-      password: 'passer', // Mot de passe MySQL
-      database: 'gestion_examens', // Nom de la base de données
-      host: 'localhost',           // Hôte de la base de données
-      dialect: 'mysql',            // Type de base de données
-    },
-  };
